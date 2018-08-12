0ad98d6e60aab9a0a2cf448f89fc8c2645e13400
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-12 01:42:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-equire_relative './spec_helper.rb'
+require_relative './spec_helper.rb'
  
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
