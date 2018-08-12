d2f7de5bf4a3d7de6bc80d8a746375d41256b2f7
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-12 02:12:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,13 @@
 # See README.md for instructions on how to do this
 require 'pry'
 
-
+def fizzbuzz?(num)
+  case
+  when num % 15 == 0 then "FizzBuzz"
+  when num % 3  == 0 then "Fizz"
+  when num % 5  == 0 then "Buzz"
+  else num
+  end
+end
 
 binding.pry
